a40789cefcd1c95885436eb430620cf0caccd6a4
Natalia Veríssimo <nattaliaverissimocosta@hotmail.com>
2022-06-13 21:54:22 -0300

    Funcionalidade de verificar disponibilidade de assentos

--- js/script.js
+++ js/script.js
@@ -16,8 +16,38 @@ function renderizaFilmes(filmes) {
             <div class="movie">
                 <img src=${filmes[i].imagem}>
                 <div class="title">${filmes[i].titulo}</div>
+                <button onclick=comprar(${filmes[i].id})>
+                    Comprar
+                    <ion-icon name="cart-outline"></ion-icon>
+                </button>
             </div>
         `
     }
 }
 
+
+function comprar(idFilme) {
+    const nome = prompt('Qual é o seu nome?')
+    const quantidade = prompt('Quantos assentos?')
+    const dadosDaCompra = {
+        nome,
+        quantidade
+    }
+
+    let promessa = axios.post(
+        `https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes/${idFilme}/ingresso`,
+        dadosDaCompra
+    )
+
+    promessa.then(compraEfetuada)
+    promessa.catch(erroNaCompra)
+
+    function compraEfetuada() {
+        alert('Ingresso comprado')
+    }
+
+    function erroNaCompra() {
+        alert('Sessão esgotada :( ')
+    }
+
+}
